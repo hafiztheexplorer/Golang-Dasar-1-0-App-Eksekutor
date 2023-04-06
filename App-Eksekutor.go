89c363d1d3bb388ef02d560a4331ed7cdcb8868d
Hafiz Nur <hafiznur@live.com>
2023-04-06 16:42:56 +0700

    update kesekian kalinya v1.0.3

--- App-Eksekutor.go
+++ App-Eksekutor.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	Packagetipedata "github.com/hafiztheexplorer/Golang-Dasar-1-1-Tipe-Data"
 )
 
 func main() {
-	fmt.Println(Packagetipedata.Tipedata())
+	Packagetipedata.Tipedata()
 }
